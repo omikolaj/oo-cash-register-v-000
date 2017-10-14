9fac3eaac7173fddd7006ee909ea8ad72292e71d
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-14 17:54:32 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,11 +12,14 @@ class CashRegister
   end
 
   def apply_discount
-    const = 0.0100
-    discount_dec = self.discount * const
-    discount = self.total * discount_dec
-    self.total -= discount
-    "After the discount, the total comes to $#{self.total.to_i}."
+    if(self.discount != 0)
+      const = 0.0100
+      discount_dec = self.discount * const
+      discount = self.total * discount_dec
+      self.total -= discount
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
   end
 
 end
